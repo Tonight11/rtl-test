1a1a53edefafccae448556bfd4d893e927b0ad45
tonight11 <godofspeedman@gmail.com>
2023-06-20 21:07:23 +0300

    feat: combine all comp

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,41 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+	import { ref } from 'vue';
+
+	import Inventory from '@/components/Inventory.vue';
+	import Profile from '@/components/Profile.vue';
+	import AddItem from '@/components/AddItem.vue';
+	import Modal from '@/components/Modal.vue';
+
+	const addModal = ref(false);
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+	<main>
+		<div class="menu">
+			<div class="menu__inner">
+				<div class="menu__main">
+					<Profile @open-add="addModal = true" />
+					<Inventory />
+				</div>
+				<Modal v-if="addModal" @closeModal="addModal = false" class="add-item">
+					<AddItem />
+				</Modal>
+			</div>
+		</div>
+	</main>
 </template>
+
+<style lang="scss">
+	.menu {
+		display: flex;
+		justify-content: center;
+
+		&__inner {
+		}
+		&__main {
+			display: flex;
+			gap: 25px;
+			margin-bottom: 25px;
+		}
+	}
+</style>

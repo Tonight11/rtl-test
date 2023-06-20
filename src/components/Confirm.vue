a24158f22b6c770c9d6a3211e46338ef0b417a88
tonight11 <godofspeedman@gmail.com>
2023-06-20 21:02:17 +0300

    feat: added confirm modal & usual

---
+++ src/components/Confirm.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+	defineEmits(['cancelDelete', 'deleteItem']);
+</script>
+
+<template>
+	<div class="confirmation-dialog">
+		<slot />
+		<div class="confirmation-dialog-buttons">
+			<button class="btn btn-white" @click="$emit('cancelDelete')">
+				Отмена
+			</button>
+			<button class="btn btn-red" @click="$emit('deleteItem')">Удалить</button>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	.confirmation-dialog {
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		background: rgba(38, 38, 38, 0.6);
+		z-index: 1;
+		padding: 20px;
+
+		/* Primary Border */
+		border: 1px solid #4d4d4d;
+		backdrop-filter: blur(8px);
+	}
+	.confirmation-dialog-buttons {
+		display: flex;
+		gap: 10px;
+	}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+	defineEmits(['closeModal']);
+</script>
+
+<template>
+	<div class="modal">
+		<div class="modal__close" @click="$emit('closeModal')">X</div>
+		<slot />
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	.modal {
+		background: rgba(38, 38, 38, 0.5);
+		min-width: 250px;
+		/* Primary Border */
+		backdrop-filter: blur(8px);
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		right: 0;
+
+		&__close {
+			position: absolute;
+			top: 10px;
+			right: 10px;
+			cursor: pointer;
+			z-index: 2;
+		}
+	}
+
+	.add-item {
+		position: relative;
+		top: auto;
+		bottom: auto;
+		right: auto;
+	}
+</style>
